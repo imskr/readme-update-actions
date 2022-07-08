ee663fa33cbc3d086cc1a4a1d6411451628a198f
Shubham Kumar <shubhamkrai123@gmail.com>
2022-07-08 20:19:58 +0530

    feat: Add medium.com rss struct

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+// create new error for empty env
+var errEnvEmpty = errors.New("getenv: Environment variable empty")
+
+// Get env string from user through actions env
+// if no env variables provided throw error
+// requires key string
+// returns string, error
+func getEnvString(key string) (string, error) {
+	env_string := os.Getenv(key)
+	if env_string == "" {
+		return env_string, errEnvEmpty
+	}
+	return env_string, nil
+}
 
 func main() {
-	fmt.Println("main.go file")
+	// get the rss list from the actions env
+	rss_medium, _ := getEnvString("INPUT_RSS_LIST")
+
+	// get medium.com rss feed
+	mediumResponse, err := http.Get(rss_medium)
+	if err != nil {
+		log.Println("Error making request to medium", err)
+	}
+
+	defer mediumResponse.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(mediumResponse.Body)
+	if err != nil {
+		log.Println("Error reading response body", err)
+	}
+
+	fmt.Println(string(responseBody))
 }
---
+++ structs/rss.go
@@ -0,0 +1,52 @@
+package structs
+
+import (
+	"encoding/xml"
+)
+
+// RSS generated struct from xml rss feed
+// https://onlinetool.io/xmltogo
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Text    string   `xml:",chardata"`
+	Dc      string   `xml:"dc,attr"`
+	Content string   `xml:"content,attr"`
+	Atom    string   `xml:"atom,attr"`
+	Version string   `xml:"version,attr"`
+	Cc      string   `xml:"cc,attr"`
+	Channel struct {
+		Text        string `xml:",chardata"`
+		Title       string `xml:"title"`
+		Description string `xml:"description"`
+		Link        struct {
+			Text string `xml:",chardata"`
+			Href string `xml:"href,attr"`
+			Rel  string `xml:"rel,attr"`
+			Type string `xml:"type,attr"`
+		} `xml:"link"`
+		Image struct {
+			Text  string `xml:",chardata"`
+			URL   string `xml:"url"`
+			Title string `xml:"title"`
+			Link  string `xml:"link"`
+		} `xml:"image"`
+		Generator     string `xml:"generator"`
+		LastBuildDate string `xml:"lastBuildDate"`
+		WebMaster     string `xml:"webMaster"`
+		Item          []struct {
+			Text        string `xml:",chardata"`
+			Title       string `xml:"title"`
+			Description string `xml:"description"`
+			Link        string `xml:"link"`
+			Guid        struct {
+				Text        string `xml:",chardata"`
+				IsPermaLink string `xml:"isPermaLink,attr"`
+			} `xml:"guid"`
+			Category []string `xml:"category"`
+			Creator  string   `xml:"creator"`
+			PubDate  string   `xml:"pubDate"`
+			Updated  string   `xml:"updated"`
+			Encoded  string   `xml:"encoded"`
+		} `xml:"item"`
+	} `xml:"channel"`
+}
